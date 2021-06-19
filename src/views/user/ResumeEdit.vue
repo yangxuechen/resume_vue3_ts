a555647145fa80d5e145ed2d100dfb956588104c
“yangxuechen” <1324688478@qq.ocm>
2021-06-19 16:36:38 +0800

    pdf导出功能

--- src/views/user/ResumeEdit.vue
+++ src/views/user/ResumeEdit.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="rs-edit-box">
-    <div class="rs-edit">
+    <div class="rs-edit" id="resume">
       {{ msg }}
-      <Template01></Template01>
+      <Template01 :ref="resume"></Template01>
     </div>
     <div class="toolMenu">
       <div class="item">
         <a-button type="primary" ghost>保存</a-button>
       </div>
       <div class="item">
-        <a-button type="danger" ghost>导出pdf</a-button>
+        <a-button type="danger" ghost @click="downloadPdf">导出pdf</a-button>
       </div>
     </div>
   </div>
@@ -19,14 +19,33 @@
 import { computed, ref } from "vue";
 import route from "../../router";
 import Template01 from "../template/Template01.vue";
+import html2canvas from "html2canvas";
+import { jsPDF } from "jspdf";
 const msg2 = ref<string>("1");
-
+const resume = ref<any>();
 const msg = computed(() => {
   console.log(route.currentRoute.value.query, "currentRoute");
 
   return route.currentRoute.value.query.name;
 });
 
+const downloadPdf = () => {
+  const htmlElement = document.getElementById("resume");
+  const width: number = htmlElement?.offsetWidth || 0;
+  const height: number = htmlElement?.offsetHeight || 0;
+
+  console.log(width + " " + height);
+
+  html2canvas(htmlElement, {
+    height: htmlElement?.offsetHeight,
+    width: htmlElement?.offsetWidth,
+  }).then((canvas) => {
+    const doc = new jsPDF();
+    doc.addImage(canvas, "image/jpeg", 0, 0, 210, 210 * 1.2);
+
+    doc.save("doc.pdf");
+  });
+};
 // console.log(route.currentRoute.value.query,'route');
 </script>
 
@@ -39,7 +58,7 @@ const msg = computed(() => {
     width: 800px;
     height: auto;
     overflow: auto;
-    //  background-color: chocolate;
+    // background-color: chocolate;
     margin-top: 25px;
     margin-bottom: 50px;
     border: 5px #dff0fe solid;
